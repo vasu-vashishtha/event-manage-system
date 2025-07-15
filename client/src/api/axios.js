549b29a11f4b0148fe9e34b7db16f9a42ae7b7d2
vasu <vashishthavasu2@gmail.com>
2025-07-15 20:52:40 +0530

    Update axios baseURL and add Vite env variable support

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+ baseURL: import.meta.env.VITE_BACKEND_URL + '/api',
 });
 
 export default instance;
